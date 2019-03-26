18196dc6395a8b4bada324c6c8f193c9f35899af
Devatanu Deka <deva.deka@gmail.com>
2019-03-26 12:48:15 +1100

    FruitAdder working with conext and dispatch

--- src/FruitAdder.js
+++ src/FruitAdder.js
@@ -1,18 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import { StoreContext } from './Store';
 
 const FruitAdder = () => {
 
   const [inputFruit, setInputFruit] = useState('');
+  const [_, dispatch] = useContext(StoreContext);
 
   const handleInputChange = (evt) => {
     setInputFruit(evt.target.value.trim());
   }
 
+  const handleAddFruit = () => {
+    dispatch({type: 'addFruit', payload:inputFruit});
+    setInputFruit("");
+  }
+
   return(
     <div>
-      <input type='text' onChange={handleInputChange}/>
+      <input type='text' value={inputFruit} onChange={handleInputChange}/>
       <br />
-      <input type='button' value='Add Fruit' disabled={inputFruit.length===0}/>
+      <input type='button' value='Add Fruit' 
+        disabled={inputFruit.length===0} 
+        onClick={ handleAddFruit }/>
     </div>
   )
 }
